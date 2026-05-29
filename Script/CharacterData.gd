237d4f3bce814bdc288085574a4b0c18d45c507c
varlike0013 <varlike0013@outlook.com>
2026-05-29 19:56:05 +0800

    update playerdata

--- Script/CharacterData.gd
+++ Script/CharacterData.gd
@@ -5,7 +5,6 @@ class_name CharacterData
 @export var group_target: String = "Character"
 @export var character_name: String = "name"
 @export_group("BasicAttributes")
-@export var character_level: int = 1
 @export var speed: float = 100.0
 @export var health: Vector2 = Vector2(100, 100)    # (当前HP, 最大HP)
 @export var magic: Vector2 = Vector2(50, 50)       # (当前MP, 最大MP)
@@ -14,6 +13,12 @@ class_name CharacterData
 @export var weight: float = 10.0
 @export var is_fly: bool = false
 @export var fly_height: float = 50.0
+@export_group("level")
+@export var character_level: int = 1
+@export var level_health_base:float = 100.0
+@export var level_health_growth:float = 10.0
+@export var level_magic_base:float = 50.0
+@export var level_magic_growth:float = 5.0
 @export_group("Percentage")
 @export_range(0, 1) var percentage_resistance: float = 0.0 ##百分减伤
 @export_range(0, 1) var percentage_bonus: float = 0.0 ##百分比增伤
@@ -37,12 +42,16 @@ func set_health_max(value:float):
 	health.y = value
 	if health.x>health.y:
 		health.x = health.y
+func update_health():
+	set_health_max(level_health_base+(character_level-1)*level_health_growth)
 func reduce_magic(value:float):
 	magic.x -= value
 	magic.x = max(0,magic.x)
 func restore_magic(value:float):
 	magic.x += value
 	magic.x = min(magic.y,magic.x)
+func update_magic():
+	set_magic_max(level_magic_base+(character_level-1)*level_magic_growth)
 func is_magic_enough(value:float) ->bool:
 	if magic.x>=value:
 		return true
@@ -70,13 +79,15 @@ func append_bonus_array(value:float): ##value in range(0,1)
 	bonus_array.append(value)
 func remove_bonus_array(value:float): ##value in range(0,1)
 	bonus_array.erase(value)
+func update_bonus():
+	percentage_bonus = resistance_array_to_percentage(bonus_array)
 func resistance_array_to_percentage(array:Array[float]): ##return in range(0,1)
 	var result:float = 1.0
 	for ar in array:
 		result = result*(1.0-ar)
 	return result
-func attribute_base_growth(attrribut_value: int, value_base: float, array_growth: Array[Vector2]) -> float:##返回具体数值如血量等
-	var result: float = value_base
+func attribute_base_growth(attrribut_value: float,array_growth: Array[Vector2]) -> float:##返回具体数值如血量等
+	var result: float = 0
 	for i in range(array_growth.size()):
 		var threshold: float = array_growth[i].x
 		var add_per_level: float = array_growth[i].y
@@ -87,5 +98,3 @@ func attribute_base_growth(attrribut_value: int, value_base: float, array_growth
 		if points_in_range > 0:
 			result += points_in_range * add_per_level
 	return result
-func update_bonus():
-	percentage_bonus = resistance_array_to_percentage(bonus_array)
--- Script/PlayerData.gd
+++ Script/PlayerData.gd
@@ -1,12 +1,12 @@
 extends CharacterData
 class_name PlayerData
 
-@export_group("Attributes")##vigor mind endurance strength dexterity intelligence
-@export var attribute_vigor:float = 10 			##生命力
-@export var vigor_health_base:float = 100
+@export_group("Attributes")##vigor mind strength dexterity intelligence
+@export var vigor:float = 10 			##生命力
+@export var vigor_base:float = 10
 @export var vigor_health_growth:Array[Vector2] = [Vector2(1,10),Vector2(30,15),Vector2(50,12),Vector2(80,8),Vector2(100,4)]
-@export var attribute_mind:float = 10  			##集中力
-@export var mind_magic_base:float = 50
+@export var mind:float = 10  			##集中力
+@export var mind_base:float = 10
 @export var mind_magic_growth:Array[Vector2] = [Vector2(1,5),Vector2(30,8),Vector2(50,6),Vector2(80,4),Vector2(100,3)]
 @export var attribute_strength:float = 10		##力量
 @export var strength_scaling_base:float = 50
@@ -24,3 +24,16 @@ class_name PlayerData
 @export var rating_value_strength:float = 10
 @export var rating_value_dexterity:float = 10
 @export var rating_value_intelligence:float = 10
+
+func update_health():
+	var result:float = level_health_base+(character_level-1)*level_health_growth
+	result += attribute_base_growth(vigor,vigor_health_growth)
+	set_health_max(result)
+func update_magic():
+	var result:float = level_magic_base+(character_level-1)*level_magic_growth
+	result += attribute_base_growth(mind,mind_magic_growth)
+	set_magic_max(result)
+func update_attributes():
+	##update vigor
+	vigor = vigor_base+character_level
+	pass
