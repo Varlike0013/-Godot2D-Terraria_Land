d3ccea80577a250b586cc08e4a3f7841396134bc
varlike0013 <varlike0013@outlook.com>
2026-06-02 19:59:18 +0800

    move file

--- Script/CharacterData.gd
+++ Script/CharacterData.gd
@@ -7,18 +7,21 @@ class_name CharacterData
 @export_group("BasicAttributes")
 @export var speed: float = 100.0
 @export var health: Vector2 = Vector2(100, 100)    # (当前HP, 最大HP)
+@export var health_base:float = 100.0
 @export var magic: Vector2 = Vector2(50, 50)       # (当前MP, 最大MP)
+@export var magic_base:float = 50.0
 @export var defense: float = 0.0 ##固定减伤
+@export var defense_base:float = 0.0
 @export var bonus:float = 0.0 ##固定增伤
-@export var weight: float = 10.0
+@export var bonus_base:float = 0.0
 @export var is_fly: bool = false
 @export var fly_height: float = 50.0
 @export_group("level")
 @export var character_level: int = 1
-@export var level_health_base:float = 100.0
-@export var level_health_growth:float = 10.0
-@export var level_magic_base:float = 50.0
-@export var level_magic_growth:float = 5.0
+@export var level_growth_health:float = 10.0
+@export var level_growth_magic:float = 5.0
+@export var level_growth_bonus:float = 5.0
+@export var level_growth_defense:float = 3.0
 @export_group("Percentage")
 @export_range(0, 1) var percentage_resistance: float = 0.0 ##百分减伤
 @export_range(0, 1) var percentage_bonus: float = 0.0 ##百分比增伤
@@ -28,7 +31,7 @@ class_name CharacterData
 @export var attack_range: float = 1.0
 @export var attack_interval: float = 1.0
 @export_group("Repelled")
-@export var repeled_speed: float = 1.0 ##击退常数，决定被击退时的速度repeled_speed = repeled_value/weight
+@export var repeled_speed: float = 1.0 ##击退常数，决定被击退时的距离
 var resistance_array:Array[float] = []
 var bonus_array:Array[float] = []
 
@@ -42,16 +45,12 @@ func set_health_max(value:float):
 	health.y = value
 	if health.x>health.y:
 		health.x = health.y
-func update_health():
-	set_health_max(level_health_base+(character_level-1)*level_health_growth)
 func reduce_magic(value:float):
 	magic.x -= value
 	magic.x = max(0,magic.x)
 func restore_magic(value:float):
 	magic.x += value
 	magic.x = min(magic.y,magic.x)
-func update_magic():
-	set_magic_max(level_magic_base+(character_level-1)*level_magic_growth)
 func is_magic_enough(value:float) ->bool:
 	if magic.x>=value:
 		return true
@@ -73,14 +72,10 @@ func append_resistance_array(value:float): ##value in range(0,1)
 	resistance_array.append(value)
 func remove_resistance_array(value:float): ##value in range(0,1)
 	resistance_array.erase(value)
-func update_resistance():
-	percentage_resistance = resistance_array_to_percentage(resistance_array)
 func append_bonus_array(value:float): ##value in range(0,1)
 	bonus_array.append(value)
 func remove_bonus_array(value:float): ##value in range(0,1)
 	bonus_array.erase(value)
-func update_bonus():
-	percentage_bonus = resistance_array_to_percentage(bonus_array)
 func resistance_array_to_percentage(array:Array[float]): ##return in range(0,1)
 	var result:float = 1.0
 	for ar in array:
@@ -98,5 +93,18 @@ func attribute_base_growth(attrribut_value: float,array_growth: Array[Vector2]) 
 		if points_in_range > 0:
 			result += points_in_range * add_per_level
 	return result
+func update_health():
+	set_health_max(health_base+(character_level-1)*level_growth_health)
+func update_magic():
+	set_magic_max(magic_base+(character_level-1)*level_growth_magic)
+func update_bonus():
+	bonus = bonus_base+(character_level-1)*level_growth_bonus
+	percentage_bonus = resistance_array_to_percentage(bonus_array)
+func update_defense():
+	defense = defense_base+(character_level-1)*level_growth_defense
+	percentage_resistance = resistance_array_to_percentage(resistance_array)
 func update_data():
-	pass
+	update_health()
+	update_magic()
+	update_defense()
+	update_bonus()
